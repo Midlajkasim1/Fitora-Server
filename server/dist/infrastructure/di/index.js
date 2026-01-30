ccf305cb1cbef3d2c646c85a142f229d38d3c326
mithu4747 <midlajmithu16744@gmail.com>
2026-01-30 19:00:32 +0530

    chore: remove dist from git tracking

--- server/dist/infrastructure/di/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.controllers = void 0;
-const admin_controllers_1 = require("./admin/admin.controllers");
-const user_controllers_1 = require("./user/user.controllers");
-exports.controllers = {
-    ...admin_controllers_1.adminControllers,
-    ...user_controllers_1.userControllers,
-};
--- server/dist/infrastructure/di/user/user.repositories.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.userRepositories = void 0;
-const user_repository_1 = require("@/infrastructure/database/repositories/user.repository");
-exports.userRepositories = {
-    userRepository: new user_repository_1.UserRepository(),
-};
