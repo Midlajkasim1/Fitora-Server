a753ba9c48982305cafb10aaaa3a90f901fc8701
mithu4747 <midlajmithu16744@gmail.com>
2026-05-16 10:00:40 +0530

    Testing full CI/CD pipeline

--- server/src/presentation/express/setting/app.ts
+++ server/src/presentation/express/setting/app.ts
@@ -25,7 +25,8 @@ app.use(compression());
 app.post("/api/user/webhook",express.raw({type:"application/json"}),asyncHandler((req:Request,res:Response)=>
     userControllers.userSubscriptionController.handlewebhook(req,res)
 ));
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 app.use("/api", routes);
 app.use(errorHandler);
